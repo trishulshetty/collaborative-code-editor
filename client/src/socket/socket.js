f242aaabbb01f1f04dc8e7710b3a176303ccbd84
Trishul S <trishulshettygs@gmail.com>
2026-05-11 14:52:11 +0530

    Configure frontend socket server URL

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000", {
+const SERVER_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:5000";
+
+export const socket = io(SERVER_URL, {
   autoConnect: true,
   transports: ["websocket", "polling"]
 });
